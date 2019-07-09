2efaf407f4ba3c2e0b4b269699aa001ee74e5af1
Olly Monger <olly@yllo.co.uk>
2019-07-09 14:48:08 +0100

    stashing changes

---
+++ src/server/server.ts
@@ -0,0 +1,99 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('/ has been accessed');
+});
+
+interface BlogPostModel {
+    id: number,
+    title: string,
+    text: string
+}
+
+const posts: BlogPostModel[] = [
+    {
+        id: 7,
+        title: "New type of cheese invented",
+        text: "This cheese is the cheesiest of all cheeses"
+    },
+    {
+        id: 1,
+        title: "Dogs can't look up",
+        text: "A little known fact about dogs is that it is impossible for them to look up"
+    },
+    {
+        id: 2,
+        title: "POST 2",
+        text: "123 531411212 asbhdashidha sidjiasj dasjd jasd uancxucnoansfuhasdnasnduansdnasudnasnduasd"
+
+    },
+    {
+        id: 3,
+        title: "POST 3 ",
+        text: "thaihisahdishaid asi diasd iashdiash diashdihaid haidh aisd haisdhias hdias hdiashdihasidh asid isadhia shdiashdi ahsidha isdh "
+    }
+];
+
+app.get('/posts/', (req, res) => {
+    const allPosts = posts;
+
+    console.log(posts)
+
+    res.send(allPosts);
+})
+
+app.get('/posts/:id', (req, res) => {
+    const parsedId = parseInt(req.params.id);
+    const matchingPost = posts.find(post => post.id === parsedId);
+
+    if (matchingPost === undefined) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.send(matchingPost);
+})
+
+// app.get('/users', function(req, res){
+//     var name = "hi there";
+//     var age = 24;
+
+//     var person = {
+//         name: name,
+//         age: age
+//     };
+
+//     savePersonToUsersFolder(person, function(err){
+//         if(err){
+//             res.status(404).send('User not saved');
+//             return;
+//         }
+//         res.send('User saved')
+//     });
+
+//     loadPerson(person, function(err){
+//         if(err){          
+//             res.status(404).send('User not loaded');
+//             return;
+//         }
+//         res.send(person.name);
+//     });
+// })
+
+// function savePersonToUsersFolder(person, callback){
+//     fs.writeFile('./users/person.json', JSON.stringify(person),callback);
+// }
+
+// function loadPerson(person, callback){
+//     fs.open('./users/person.json', 'as', callback);
+// }
+
+
+app.listen(port, err => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(`server is listening on ${port}`)
+})
